f4bbe061f320d22d678f95fe213451b0497d4394
Komaron <kjames953@gmail.com>
2020-01-12 23:02:13 -0500

    Creating revenue calculator service class and updates to sale model

--- app/models/acme/sale.rb
+++ app/models/acme/sale.rb
@@ -1,4 +1,7 @@
 module Acme
   class Sale < ApplicationRecord
+
+    validates :item_price, presence: true
+    validates :quantity, presence: true
   end
 end
---
+++ app/services/acme/revenue_calculator.rb
@@ -0,0 +1,14 @@
+module Acme
+  class RevenueCalculator
+
+    def calculate
+      sales.sum{ |sale| sale.quantity * sale.item_price.to_f }
+    end
+
+    private
+
+    def sales
+      Acme::Sale.all
+    end
+  end
+end
