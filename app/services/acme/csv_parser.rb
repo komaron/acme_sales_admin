2bde7891b0f2c96afddbdfda892035892c47e7d6
Komaron <kjames953@gmail.com>
2020-01-12 19:07:01 -0500

    Building CSV parser service class

--- app/services/acme/csv_parser.rb
+++ app/services/acme/csv_parser.rb
@@ -1,15 +1,28 @@
+require 'csv'
+
 module Acme
   class CsvParser
     attr_reader :data_file
 
-    def initalize(options)
-      @data_file = options[:data_file]
+    def initialize(options)
+      @data_file = options[:data_file] || default_data_file
     end
 
     def import!
       CSV.foreach(data_file, headers: true) do |row|
-        puts row.inspect
+        Acme::Sale.create(
+          customer_name:      row['Customer Name'],
+          item_description:   row['Item Description'],
+          item_price:         row['Item Price'],
+          quantity:           row['Quantity'],
+          merchant_name:      row['Merchant Name'],
+          merchant_address:   row['Merchant Address']
+        )
       end
+    end
+
+    def default_data_file
+      Rails.root.join('salesdata.csv')
     end
   end
 end
