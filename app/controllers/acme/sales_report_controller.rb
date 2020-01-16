68419215e372a454a21f51a6dc7b5c4c35c0d811
Komaron <kjames953@gmail.com>
2020-01-16 18:17:33 -0500

    Removing commented code

--- app/controllers/acme/sales_report_controller.rb
+++ app/controllers/acme/sales_report_controller.rb
@@ -26,45 +26,6 @@ module Acme
 
       redirect_to '/'
     end
-    # # POST /acme/sales
-    # # POST /acme/sales.json
-    # def create
-    #   @report = Acme::SaleReport.new(report_params)
-
-    #   respond_to do |format|
-    #     if @report.save
-    #       format.html { redirect_to @report, notice: 'Report was successfully created.' }
-    #       format.json { render :show, status: :created, location: @report }
-    #     else
-    #       format.html { render :new }
-    #       format.json { render json: @report.errors, status: :unprocessable_entity }
-    #     end
-    #   end
-    # end
-
-    # # PATCH/PUT /acme/sales/1
-    # # PATCH/PUT /acme/sales/1.json
-    # def update
-    #   respond_to do |format|
-    #     if @report.update(report_params)
-    #       format.html { redirect_to @report, notice: 'Report was successfully updated.' }
-    #       format.json { render :show, status: :ok, location: @report }
-    #     else
-    #       format.html { render :edit }
-    #       format.json { render json: @report.errors, status: :unprocessable_entity }
-    #     end
-    #   end
-    # end
-
-    # # DELETE /acme/sales/1
-    # # DELETE /acme/sales/1.json
-    # def destroy
-    #   @report.destroy
-    #   respond_to do |format|
-    #     format.html { redirect_to reports_url, notice: 'Report was successfully destroyed.' }
-    #     format.json { head :no_content }
-    #   end
-    # end
 
     private
 
